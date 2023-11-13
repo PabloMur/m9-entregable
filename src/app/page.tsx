e768ac88a233accd7b6124298d537b514052aa51
PabloMur <polillomurillo@gmail.com>
2023-11-13 12:10:01 -0300

    add some api keys

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,6 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
-  (function () {
-    console.log("hola");
-    console.log(process.env.SECRECT);
-  })();
   return (
     <main className={styles.main}>
       <div className={styles.description}>
