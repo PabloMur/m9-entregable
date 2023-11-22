3825c4517aaf7cdb607ce38e69a6019b0a4529d6
PabloMur <polillomurillo@gmail.com>
2023-11-22 15:25:52 -0300

    create order en proceso

--- src/app/api/order/route.tsx
+++ src/app/api/order/route.tsx
@@ -1,3 +1,13 @@
 //Este endpoint es el que debe crear una order y recibe el id del producto
 //Usa el metodo post
+import { OrderController } from "@/controllers/OrderController";
+import { NextRequest, NextResponse } from "next/server";
+export async function POST(request: NextRequest) {
+  try {
+    const response = await OrderController.createOrder(request);
+    return NextResponse.json({ response });
+  } catch (error) {
+    console.error(error);
+  }
+}
 export {};
